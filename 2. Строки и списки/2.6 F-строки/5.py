be8ce4d33990ccd629874b9e8905d3f6f0de3c0f
RAX3R <45417916+vqpv@users.noreply.github.com>
2021-09-17 22:54:17 +0300

    2.6 - 5

---
+++ 2. Строки и списки/2.6 F-строки/5.py
@@ -0,0 +1,9 @@
+num_1, num_2 = int(input()), int(input())
+
+result_1 = num_1 / num_2
+result_2 = num_1 // num_2
+result_3 = num_1 % num_2
+
+print(f'{num_1} / {num_2} = {result_1}')
+print(f'{num_1} // {num_2} = {result_2}')
+print(f'{num_1} % {num_2} = {result_3}')
