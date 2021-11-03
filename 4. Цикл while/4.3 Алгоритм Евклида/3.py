1de2d8720df339cc7e9e2107b5120ae82fb30231
RAX3R <45417916+vqpv@users.noreply.github.com>
2021-11-03 16:27:20 -0400

    4.3 - 3

---
+++ 4. Цикл while/4.3 Алгоритм Евклида/3.py
@@ -0,0 +1,15 @@
+
+a, b = map(int, input().split())
+
+c, d = a, b
+
+while a != 0 and b != 0:
+    if a > b:
+        a %= b
+    else:
+        b %= a
+
+if a > b:
+    print((c * d) // a)
+else:
+    print((c * d) // b)
