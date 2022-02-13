4ba5f878a12e375a0e2274ffe2b8664e17bccf70
RAX3R <45417916+vqpv@users.noreply.github.com>
2022-02-13 14:45:16 -0500

    10.7 - 4

---
+++ 10. Advanced Python/10.7 Сортировка по ключу Python. Аргумент key/4.py
@@ -0,0 +1,12 @@
+models = [{'make': 'Nokia', 'model': 216, 'color': 'Black'},
+          {'make': 'Mi Max', 'model': 2, 'color': 'Gold'},
+          {'make': 'Samsung', 'model': 7, 'color': 'Blue'},
+          {'make': 'Apple', 'model': 10, 'color': 'Silver'},
+          {'make': 'Oppo', 'model': 9, 'color': 'Red'},
+          {'make': 'Huawei', 'model': 4, 'color': 'Grey'},
+          {'make': 'Honor', 'model': 3, 'color': 'Black'}]
+
+sorted_models = sorted(models, key=lambda x: x['color'])
+
+for model in sorted_models:
+    print(f"Производитель: {model['make']}, модель: {model['model']}, цвет: {model['color']}")
