3cbaeb8c7efc88634f48bb711d84c88d64f3b1a8
vqpv <45417916+vqpv@users.noreply.github.com>
2022-02-17 15:56:43 -0500

    1.5 - update

--- 1. Первые шаги/1.5 Функция input()/7.py
+++ 1. Первые шаги/1.5 Функция input()/7.py
@@ -1,3 +1,4 @@
-a, b = map(float, input().split())
+h1, m1, s1 = int(input()), int(input()), int(input())
+h2, m2, s2 = int(input()), int(input()), int(input())
 
-print(abs(a - b))
+print(60 * 60 * (h2 - h1) + 60 * (m2 - m1) + (s2 - s1))
--- 1. Первые шаги/1.5 Функция input()/9.py
+++ 1. Первые шаги/1.5 Функция input()/9.py
@@ -1,4 +1,1 @@
-h1, m1, s1 = int(input()), int(input()), int(input())
-h2, m2, s2 = int(input()), int(input()), int(input())
-
-print(60 * 60 * (h2 - h1) + 60 * (m2 - m1) + (s2 - s1))
+print(abs(int(input())) + abs(int(input())))
